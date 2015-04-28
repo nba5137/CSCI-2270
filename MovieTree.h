871fbd9915190e968fd19b9cd4e52f53c0980a6f
nba5137 <siso8575@colorado.edu>
2015-04-28 09:53:05 -0600

    Update MovieTree.h

--- MovieTree.h
+++ MovieTree.h
@@ -1,43 +1,57 @@
 #ifndef MOVIETREE_H
 #define MOVIETREE_H
 
+#include<string>
 
-struct MovieNode{
+struct MovieNode
+{
+  MovieNode();
+  MovieNode(int& rank, std::string& ttl, int& yr, int& qtty);
+  virtual ~MovieNode();
+  friend std::ostream& operator<<(std::ostream& os, MovieNode* n);
+
+  MovieNode* parent;
+  MovieNode* left;
+  MovieNode* right;
     int ranking;
     std::string title;
     int year;
     int quantity;
-    MovieNode *parent;
-    MovieNode *leftChild;
-    MovieNode *rightChild;
-
-    MovieNode(){};
-
-    MovieNode(int in_ranking, std::string in_title, int in_year, int in_quantity)
-    {
-        ranking = in_ranking;
-        title = in_title;
-        year = in_year;
-        quantity = in_quantity;
-    }
-
 };
+
 
 class MovieTree
 {
-    public:
-        MovieTree();
-        virtual ~MovieTree();
-        void printMovieInventory();
-        void addMovieNode(int ranking, std::string title, int releaseYear, int quantity);
-        void findMovie(std::string title);
-        void rentMovie(std::string title);
-    protected:
-    private:
-        void DeleteAll(MovieNode * node);
-        void printMovieInventory(MovieNode * node);
-        MovieNode* searchMovieTree(MovieNode * node, std::string title);
-        MovieNode *root;
+public:
+  MovieTree();
+  virtual ~MovieTree();
+
+  inline void test()
+  {
+    delete_tree(root);
+  }
+
+  void addRawNode(int&, std::string&, int&, int&);
+  void insert(MovieNode*);        
+  void insert(MovieNode*, MovieNode*);  
+  void inorder_walk();          
+  MovieNode* search(std::string&);    
+  MovieNode* iterative_search(std::string&); 
+  MovieNode* minimum();         
+  MovieNode* maximum();         
+
+private:
+  MovieNode *root;
+
+  void inorder_walk(MovieNode*);      
+  MovieNode* search(MovieNode*, std::string&);
+  MovieNode* minimum(MovieNode*);     
+  MovieNode* maximum(MovieNode*);     
+  MovieNode* successor(MovieNode*);   
+  MovieNode* predecessor(MovieNode*);   
+  void transplant(MovieNode*, MovieNode*);
+  void delete_node(MovieNode*);     
+  void delete_tree(MovieNode*);     
 };
 
 #endif // MOVIETREE_H
